fa6db9d4f68a743cd40e144d41cce996d008598d
Ben Norris <ben@bsn.io>
2016-02-10 16:29:42 -0700

    Add blank framework

---
+++ MultiAuth/MultiAuth.h
@@ -0,0 +1,19 @@
+//
+//  MultiAuth.h
+//  MultiAuth
+//
+//  Created by Ben Norris on 2/10/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultiAuth.
+FOUNDATION_EXPORT double MultiAuthVersionNumber;
+
+//! Project version string for MultiAuth.
+FOUNDATION_EXPORT const unsigned char MultiAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiAuth/PublicHeader.h>
+
+
