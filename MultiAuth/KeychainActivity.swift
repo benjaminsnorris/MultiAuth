ffe5a454bc22b20a0bf24a394cf0b9fb95ab839f
Ben Norris <ben@bsn.io>
2016-02-10 17:03:04 -0700

    Bring over file contents from original project

--- MultiAuth/KeychainActivity.swift
+++ MultiAuth/KeychainActivity.swift
@@ -6,4 +6,79 @@
 //  Copyright © 2016 BSN Design. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class KeychainActivity: UIActivity {
+    
+    // MARK: - Public properties
+    
+    var handler: ((username: String?, password: String?, errorMessage: String?) -> ())?
+    
+    
+    // MARK: - Required overrides
+    
+    override func activityType() -> String? {
+        return AuthenticationService.findLoginAction
+    }
+    
+    override func activityTitle() -> String? {
+        return "Keychain"
+    }
+    
+    override func activityImage() -> UIImage? {
+        return UIImage(named: "Key")
+    }
+    
+    override func canPerformWithActivityItems(activityItems: [AnyObject]) -> Bool {
+        if let extensionItem = activityItems.first as? NSExtensionItem {
+            if let itemProvider = extensionItem.attachments?.first as? NSItemProvider where itemProvider.hasItemConformingToTypeIdentifier(AuthenticationService.findLoginAction) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    override class func activityCategory() -> UIActivityCategory {
+        return .Action
+    }
+    
+    override func performActivity() {
+        SecRequestSharedWebCredential(.None, .None) { (credentials, error) -> Void in
+            guard error == nil else {
+                print("error loading safari credentials: \(error)")
+                self.finishActivity(username: nil, password: nil, success: false)
+                return
+            }
+            
+            guard let unwrappedCredentials = credentials else {
+                print("Error unwrapping credentials array", credentials)
+                self.finishActivity(username: nil, password: nil, success: false)
+                return
+            }
+            let arrayCredentials = unwrappedCredentials as [AnyObject]
+            guard let typedCredentials = arrayCredentials as? [[String: AnyObject]] else {
+                print("error typecasting credentials")
+                self.finishActivity(username: nil, password: nil, success: false)
+                return
+            }
+            
+            if let credential = typedCredentials.first, username = credential[kSecAttrAccount as String] as? String, password = credential[kSecSharedPassword as String] as? String {
+                AuthenticationService.recordLogInViaSharedCredentials()
+                self.finishActivity(username: username, password: password, success: true)
+            }
+        }
+    }
+    
+}
+
+
+// MARK: - Private functions
+
+private extension KeychainActivity {
+    
+    func finishActivity(username username: String?, password: String?, success: Bool) {
+        handler?(username: username, password: password, errorMessage: success ? nil : "No saved passwords found")
+        self.activityDidFinish(success)
+    }
+    
+}
--- MultiAuth/MultiAuthService.swift
+++ MultiAuth/MultiAuthService.swift
@@ -6,4 +6,119 @@
 //  Copyright © 2016 BSN Design. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct AuthenticationService {
+    
+    // MARK: - Public properties
+    
+    var networkAccess: AuthenticationNetworkAccess = AuthenticationNetworkAPIAccess()
+    var handler: ((username: String?, password: String?, errorMessage: String?) -> ())?
+    
+    
+    // MARK: - Public constants
+    
+    static let findLoginAction = "org.appextension.find-login-action"
+    static let URLStringKey = "url_string"
+    
+    
+    // MARK: - Constants
+    
+    private static let usernameKey = "username"
+    private static let passwordKey = "password"
+    private static let logInViaSharedCredentialsKey = "logInViaSharedCredentials"
+    
+    
+    // MARK: - Public API
+    
+    func logIn(username: String, password: String, completion: (error: String?) -> Void) {
+        networkAccess.logIn(username, password: password) { success, error in
+            if success && error == nil {
+                self.saveSharedCredentials(username: username, password: password)
+                completion(error: nil)
+            } else if let error = error as? NSError {
+                completion(error: error.localizedDescription)
+            } else {
+                completion(error: "Unknown error")
+            }
+        }
+    }
+    
+    func logOut() {
+        networkAccess.logOut()
+    }
+    
+    func retrieveCredentials(URLString: String, viewController: UIViewController, sender: AnyObject) {
+        let activityViewController = configuredActivityViewController(URLString, sender: sender)
+        activityViewController.completionWithItemsHandler = { activityType, completed, returnedItems, activityError in
+            if let returnedItems = returnedItems, extensionItem = returnedItems.first as? NSExtensionItem, itemProvider = extensionItem.attachments?.first as? NSItemProvider where itemProvider.hasItemConformingToTypeIdentifier(kUTTypePropertyList as String) {
+                itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil) { itemDictionary, itemProviderError in
+                    if let itemDictionary = itemDictionary as? [String: String] where itemProviderError == nil {
+                        let username = itemDictionary[AuthenticationService.usernameKey]
+                        let password = itemDictionary[AuthenticationService.passwordKey]
+                        self.handler?(username: username, password: password, errorMessage: nil)
+                    } else {
+                        print("status=failed-to-load-item error=\(itemProviderError)")
+                    }
+                }
+            } else {
+                print("status=failed-to-find-login URLString=\(URLString) error=\(activityError)")
+                self.handler?(username: nil, password: nil, errorMessage: nil)
+            }
+        }
+        viewController.presentViewController(activityViewController, animated: true, completion: nil)
+    }
+    
+    static func recordLogInViaSharedCredentials() {
+        NSUserDefaults.standardUserDefaults().setBool(true, forKey: logInViaSharedCredentialsKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+}
+
+
+// MARK: - Private functions
+
+private extension AuthenticationService {
+    
+    func configuredActivityViewController(URLString: String, sender: AnyObject) -> UIActivityViewController {
+        let item = [AuthenticationService.URLStringKey: URLString]
+        let itemProvider = NSItemProvider(item: item, typeIdentifier: AuthenticationService.findLoginAction)
+        let extensionItem = NSExtensionItem()
+        extensionItem.attachments = [itemProvider]
+        let keychainActivity = KeychainActivity()
+        keychainActivity.handler = handler
+        let activityViewController = UIActivityViewController(activityItems: [extensionItem], applicationActivities: [keychainActivity])
+        if let barButtonItem = sender as? UIBarButtonItem {
+            activityViewController.popoverPresentationController?.barButtonItem = barButtonItem
+        } else if let senderView = sender as? UIView {
+            activityViewController.popoverPresentationController?.sourceView = senderView.superview
+            activityViewController.popoverPresentationController?.sourceRect = senderView.frame
+        }
+        return activityViewController
+    }
+    
+    func saveSharedCredentials(username username: String, password: String) {
+        if AuthenticationService.didRecordLogInViaSharedCredentials() { return }
+        guard let URL = NSURL(string: AppState.currentEnvironment.rawValue) else { fatalError("Invalid URL") }
+        let baseURL: NSURL
+        if let _baseURL = URL.baseURL {
+            baseURL = _baseURL
+        } else {
+            baseURL = URL
+        }
+        guard let components = NSURLComponents(URL: baseURL, resolvingAgainstBaseURL: false), host = components.host else { fatalError("Could not get host from URL:\(baseURL)") }
+        SecAddSharedWebCredential(host, username, password) { error in
+            if let error = error {
+                print("status=failed-to-save-credentials error=\(error)")
+            } else {
+                AuthenticationService.recordLogInViaSharedCredentials()
+            }
+        }
+    }
+    
+    static func didRecordLogInViaSharedCredentials() -> Bool {
+        return NSUserDefaults.standardUserDefaults().boolForKey(logInViaSharedCredentialsKey)
+    }
+    
+}
